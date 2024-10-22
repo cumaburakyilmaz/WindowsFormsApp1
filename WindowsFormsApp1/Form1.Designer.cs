bbc7723757712b044e490b57236ae41a87f2a3f3
sino <sino@TSC>
2024-10-22 22:15:09 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBPablo = new System.Windows.Forms.PictureBox();
+            this.pBSandro = new System.Windows.Forms.PictureBox();
+            this.pBLeonardo = new System.Windows.Forms.PictureBox();
+            this.pBJohannes = new System.Windows.Forms.PictureBox();
+            this.pBAndy = new System.Windows.Forms.PictureBox();
+            this.pBMichelaangelo = new System.Windows.Forms.PictureBox();
+            this.lblLeo = new System.Windows.Forms.Label();
+            this.lblMic = new System.Windows.Forms.Label();
+            this.lblSan = new System.Windows.Forms.Label();
+            this.lblJoh = new System.Windows.Forms.Label();
+            this.lblAnd = new System.Windows.Forms.Label();
+            this.lblPab = new System.Windows.Forms.Label();
+            this.btnLeonardo = new System.Windows.Forms.Button();
+            this.btnMichelangelo = new System.Windows.Forms.Button();
+            this.btnSandrro = new System.Windows.Forms.Button();
+            this.btnJohannes = new System.Windows.Forms.Button();
+            this.btnAndy = new System.Windows.Forms.Button();
+            this.btnPablo = new System.Windows.Forms.Button();
+            this.richTextBoxAciklama = new System.Windows.Forms.RichTextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBPablo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBSandro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBLeonardo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBJohannes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBAndy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBMichelaangelo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBPablo
+            // 
+            this.pBPablo.Image = global::WindowsFormsApp1.Properties.Resources.download5;
+            this.pBPablo.Location = new System.Drawing.Point(547, 235);
+            this.pBPablo.Name = "pBPablo";
+            this.pBPablo.Size = new System.Drawing.Size(156, 127);
+            this.pBPablo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBPablo.TabIndex = 5;
+            this.pBPablo.TabStop = false;
+            // 
+            // pBSandro
+            // 
+            this.pBSandro.Image = global::WindowsFormsApp1.Properties.Resources.download2;
+            this.pBSandro.Location = new System.Drawing.Point(547, 24);
+            this.pBSandro.Name = "pBSandro";
+            this.pBSandro.Size = new System.Drawing.Size(156, 127);
+            this.pBSandro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBSandro.TabIndex = 4;
+            this.pBSandro.TabStop = false;
+            // 
+            // pBLeonardo
+            // 
+            this.pBLeonardo.Image = global::WindowsFormsApp1.Properties.Resources.download;
+            this.pBLeonardo.Location = new System.Drawing.Point(93, 24);
+            this.pBLeonardo.Name = "pBLeonardo";
+            this.pBLeonardo.Size = new System.Drawing.Size(156, 127);
+            this.pBLeonardo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBLeonardo.TabIndex = 3;
+            this.pBLeonardo.TabStop = false;
+            this.pBLeonardo.Click += new System.EventHandler(this.pBLeonardo_Click);
+            // 
+            // pBJohannes
+            // 
+            this.pBJohannes.Image = global::WindowsFormsApp1.Properties.Resources.download3;
+            this.pBJohannes.Location = new System.Drawing.Point(93, 235);
+            this.pBJohannes.Name = "pBJohannes";
+            this.pBJohannes.Size = new System.Drawing.Size(156, 127);
+            this.pBJohannes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBJohannes.TabIndex = 2;
+            this.pBJohannes.TabStop = false;
+            // 
+            // pBAndy
+            // 
+            this.pBAndy.Image = global::WindowsFormsApp1.Properties.Resources.download4;
+            this.pBAndy.Location = new System.Drawing.Point(321, 235);
+            this.pBAndy.Name = "pBAndy";
+            this.pBAndy.Size = new System.Drawing.Size(156, 127);
+            this.pBAndy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBAndy.TabIndex = 1;
+            this.pBAndy.TabStop = false;
+            // 
+            // pBMichelaangelo
+            // 
+            this.pBMichelaangelo.Image = global::WindowsFormsApp1.Properties.Resources.download1;
+            this.pBMichelaangelo.Location = new System.Drawing.Point(321, 24);
+            this.pBMichelaangelo.Name = "pBMichelaangelo";
+            this.pBMichelaangelo.Size = new System.Drawing.Size(156, 127);
+            this.pBMichelaangelo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBMichelaangelo.TabIndex = 0;
+            this.pBMichelaangelo.TabStop = false;
+            // 
+            // lblLeo
+            // 
+            this.lblLeo.AutoSize = true;
+            this.lblLeo.Location = new System.Drawing.Point(93, 165);
+            this.lblLeo.Name = "lblLeo";
+            this.lblLeo.Size = new System.Drawing.Size(156, 13);
+            this.lblLeo.TabIndex = 6;
+            this.lblLeo.Text = "Leonardo da Vinci “Mona Lisa” ";
+            // 
+            // lblMic
+            // 
+            this.lblMic.AutoSize = true;
+            this.lblMic.Location = new System.Drawing.Point(303, 165);
+            this.lblMic.Name = "lblMic";
+            this.lblMic.Size = new System.Drawing.Size(190, 13);
+            this.lblMic.TabIndex = 7;
+            this.lblMic.Text = "Michelangelo, “The Creation of Adam” ";
+            // 
+            // lblSan
+            // 
+            this.lblSan.AutoSize = true;
+            this.lblSan.Location = new System.Drawing.Point(532, 165);
+            this.lblSan.Name = "lblSan";
+            this.lblSan.Size = new System.Drawing.Size(188, 13);
+            this.lblSan.TabIndex = 8;
+            this.lblSan.Text = "Sandro Botticelli, “The Birth of Venus” ";
+            // 
+            // lblJoh
+            // 
+            this.lblJoh.AutoSize = true;
+            this.lblJoh.Location = new System.Drawing.Point(62, 377);
+            this.lblJoh.Name = "lblJoh";
+            this.lblJoh.Size = new System.Drawing.Size(218, 13);
+            this.lblJoh.TabIndex = 9;
+            this.lblJoh.Text = "Johannes Vermeer, “Girl with a Pearl Earring”";
+            // 
+            // lblAnd
+            // 
+            this.lblAnd.AutoSize = true;
+            this.lblAnd.Location = new System.Drawing.Point(303, 377);
+            this.lblAnd.Name = "lblAnd";
+            this.lblAnd.Size = new System.Drawing.Size(191, 13);
+            this.lblAnd.TabIndex = 10;
+            this.lblAnd.Text = "Andy Warhol, “Campbell’s Soup Cans” ";
+            // 
+            // lblPab
+            // 
+            this.lblPab.AutoSize = true;
+            this.lblPab.Location = new System.Drawing.Point(555, 377);
+            this.lblPab.Name = "lblPab";
+            this.lblPab.Size = new System.Drawing.Size(131, 13);
+            this.lblPab.TabIndex = 11;
+            this.lblPab.Text = "Pablo Picasso, “Guernica”";
+            // 
+            // btnLeonardo
+            // 
+            this.btnLeonardo.Location = new System.Drawing.Point(122, 193);
+            this.btnLeonardo.Name = "btnLeonardo";
+            this.btnLeonardo.Size = new System.Drawing.Size(100, 23);
+            this.btnLeonardo.TabIndex = 18;
+            this.btnLeonardo.Text = "Leonardo da";
+            this.btnLeonardo.UseVisualStyleBackColor = true;
+            this.btnLeonardo.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnMichelangelo
+            // 
+            this.btnMichelangelo.Location = new System.Drawing.Point(348, 193);
+            this.btnMichelangelo.Name = "btnMichelangelo";
+            this.btnMichelangelo.Size = new System.Drawing.Size(100, 23);
+            this.btnMichelangelo.TabIndex = 19;
+            this.btnMichelangelo.Text = "Michelangelo";
+            this.btnMichelangelo.UseVisualStyleBackColor = true;
+            this.btnMichelangelo.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnSandrro
+            // 
+            this.btnSandrro.Location = new System.Drawing.Point(571, 192);
+            this.btnSandrro.Name = "btnSandrro";
+            this.btnSandrro.Size = new System.Drawing.Size(100, 24);
+            this.btnSandrro.TabIndex = 20;
+            this.btnSandrro.Text = "Sandrro Botticelli";
+            this.btnSandrro.UseVisualStyleBackColor = true;
+            this.btnSandrro.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnJohannes
+            // 
+            this.btnJohannes.Location = new System.Drawing.Point(122, 405);
+            this.btnJohannes.Name = "btnJohannes";
+            this.btnJohannes.Size = new System.Drawing.Size(100, 26);
+            this.btnJohannes.TabIndex = 21;
+            this.btnJohannes.Text = "Johannes Vemeer";
+            this.btnJohannes.UseVisualStyleBackColor = true;
+            this.btnJohannes.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnAndy
+            // 
+            this.btnAndy.Location = new System.Drawing.Point(348, 405);
+            this.btnAndy.Name = "btnAndy";
+            this.btnAndy.Size = new System.Drawing.Size(100, 26);
+            this.btnAndy.TabIndex = 22;
+            this.btnAndy.Text = "Andy Warhol";
+            this.btnAndy.UseVisualStyleBackColor = true;
+            this.btnAndy.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btnPablo
+            // 
+            this.btnPablo.Location = new System.Drawing.Point(571, 405);
+            this.btnPablo.Name = "btnPablo";
+            this.btnPablo.Size = new System.Drawing.Size(100, 26);
+            this.btnPablo.TabIndex = 23;
+            this.btnPablo.Text = "Pablo Picasso";
+            this.btnPablo.UseVisualStyleBackColor = true;
+            this.btnPablo.Click += new System.EventHandler(this.button_Click);
+            // 
+            // richTextBoxAciklama
+            // 
+            this.richTextBoxAciklama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxAciklama.Location = new System.Drawing.Point(151, 437);
+            this.richTextBoxAciklama.Name = "richTextBoxAciklama";
+            this.richTextBoxAciklama.Size = new System.Drawing.Size(471, 217);
+            this.richTextBoxAciklama.TabIndex = 24;
+            this.richTextBoxAciklama.Text = "";
+            this.richTextBoxAciklama.Visible = false;
+            this.richTextBoxAciklama.TextChanged += new System.EventHandler(this.richTextBoxAciklama_TextChanged);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(628, 437);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(101, 217);
+            this.btnReset.TabIndex = 25;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Location = new System.Drawing.Point(44, 437);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(101, 217);
+            this.btnCikis.TabIndex = 26;
+            this.btnCikis.Text = "Çıkış";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 666);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.richTextBoxAciklama);
+            this.Controls.Add(this.btnPablo);
+            this.Controls.Add(this.btnAndy);
+            this.Controls.Add(this.btnJohannes);
+            this.Controls.Add(this.btnSandrro);
+            this.Controls.Add(this.btnMichelangelo);
+            this.Controls.Add(this.btnLeonardo);
+            this.Controls.Add(this.lblPab);
+            this.Controls.Add(this.lblAnd);
+            this.Controls.Add(this.lblJoh);
+            this.Controls.Add(this.lblSan);
+            this.Controls.Add(this.lblMic);
+            this.Controls.Add(this.lblLeo);
+            this.Controls.Add(this.pBPablo);
+            this.Controls.Add(this.pBSandro);
+            this.Controls.Add(this.pBLeonardo);
+            this.Controls.Add(this.pBJohannes);
+            this.Controls.Add(this.pBAndy);
+            this.Controls.Add(this.pBMichelaangelo);
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.pBPablo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBSandro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBLeonardo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBJohannes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBAndy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBMichelaangelo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBMichelaangelo;
+        private System.Windows.Forms.PictureBox pBAndy;
+        private System.Windows.Forms.PictureBox pBJohannes;
+        private System.Windows.Forms.PictureBox pBLeonardo;
+        private System.Windows.Forms.PictureBox pBSandro;
+        private System.Windows.Forms.PictureBox pBPablo;
+        private System.Windows.Forms.Label lblLeo;
+        private System.Windows.Forms.Label lblMic;
+        private System.Windows.Forms.Label lblSan;
+        private System.Windows.Forms.Label lblJoh;
+        private System.Windows.Forms.Label lblAnd;
+        private System.Windows.Forms.Label lblPab;
+        private System.Windows.Forms.Button btnLeonardo;
+        private System.Windows.Forms.Button btnMichelangelo;
+        private System.Windows.Forms.Button btnSandrro;
+        private System.Windows.Forms.Button btnJohannes;
+        private System.Windows.Forms.Button btnAndy;
+        private System.Windows.Forms.Button btnPablo;
+        private System.Windows.Forms.RichTextBox richTextBoxAciklama;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCikis;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public decimal Toplam = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+                
+        private void button_Click(object sender, EventArgs e)
+        {
+            //Burdaki gedişkene sadece bu metod içinde ulaşmam gerektiginden local de tanımlamama gerek yok.
+            decimal secilenParaninDegeri = 0;
+            Button tıklanılanButon = (Button)sender;
+
+            //Aşagıda hangi pictureboxa tıklanmış ise ona göre secilenParaninDegerini güncelliyoruz.
+            switch (tıklanılanButon.Name)
+            {
+                case "btnLeonardo":
+                    DigerleriniGizle("btnLeonardo", "pBLeonardo");
+                    richTextBoxAciklama.Visible = true;
+                    richTextBoxAciklama.Text = "Leonardo da Vinci “Mona Lisa” ; 1503” " +
+                        " Listenin başında “Mona Lisa”nın olması eminiz ki kimseye sürpriz olmadı. Leonardo da Vinci’nin bu önemli başyapıtı, şu an dünyada en çok bilinen ve en çok ziyaret edilen sanat eseridir. “La Giaconda” olarak da bilinen eser, Floransalı varlıklı tüccar Francesco di Bartolomeo del Giocondo’nun \r\nKarısı olarak düşünülüyor. Tam olarak kimliği anlaşılamayan tablo, alternatif bir öneri olarak, Da vinci’nin annesini ve aslında kendi potresini de içeriyor.\r\n";
+                    break;
+                case "btnMichelangelo":
+                    DigerleriniGizle("btnMichelangelo", "pBMichelaangelo");   
+                    richTextBoxAciklama.Visible = true;
+                    richTextBoxAciklama.Text = "Michelangelo, “The Creation of Adam” (Adem’in Yaratılışı); 1511" +
+                        "\r\nMichelangelo’nun sanat hayatındaki en önemli eserlerinden biri olan “Adem’in Yaratılışı”, tarihte en çok tekrarlanan kutsal tablolardan biri olmuştur. Tanrı’nın gücüne olan inancına sadık kalarak onu yaşlı ama kaslı bir erkek olarak tasvir eden Michelangelo, bu tabloda, insan figürlerinden oluşan bulutun içindeki Tanrı’nın parmak ucuyla Adem’e ruh vermesiyle ilk insanın yaratılışını anlatarak aynı zamanda Tanrı’nın insan ve insanlıkla kurduğu bağı da temsil ediliyor. Bu ünlü eserin başrolündeki Adem ise sanat tarihindeki en ünlü nü çalışmalardan biri olarak biliniyor. 1508 ve 1512 yılları arasında yapılmış eser Vatikan’daki Sistina Şapeli’nde bulunuyor.\r\n";
+
+                    break;
+                case "btnSandrro":
+                    DigerleriniGizle("btnSandrro", "pBSandro");
+                    richTextBoxAciklama.Visible = true;
+                    richTextBoxAciklama.Text = "Sandro Botticelli, “The Birth of Venus” (Venüs’ün Doğuşu); 1486" +
+                        "Botticelli’nin ünlü eseri “Venüs’ün Doğuş’u”, etkili Floransa hanedanı Medici tarafından yaptırılmıştır. Mona Lisa’da yapıldığı gibi geleneksel ahşap panelleri kullanmak yerine tuval üzerine sulu boya ile resim yapmak, o dönemde popüler olan geleneksel malzemelerden bir kopma gösterir. Antik dünyayı referans alarak ortasında nü bulunan bu eser, Rönesans döneminin büyük bir hazinesidir. Yunan mitolojisinde Afrodit, Roma mitolojisinde ise Venüs olarak bilinen güzellik ve aşk tanrıçasının doğuşunu resmeden ressamın 1482 ila 1486 yılları arasında yaptığı bu ünlü eseri Floransa’daki Uffizi Galerisi’nde görebilirsiniz. \r\n";
+                    break;
+                case "btnJohannes":
+                    DigerleriniGizle("btnJohannes", "pBJohannes"); 
+                    richTextBoxAciklama.Visible = true;
+                    richTextBoxAciklama.Text = "Johannes Vermeer, “Girl with a Pearl Earring”(İnci Küpeli Kız); 1665" +
+                        " Çoğu insan tarafından “Kuzeyin Mona Lisa’sı” olarak düşünülen “İnci Küpeli Kız”, 17. yüzyılın Alman ressamı Johannes Vermeer tarafından resmedildi. Bakıldığında oldukça basit görünen bu portre aslında fazlasıyla gizemli! Başrolünde sadece inci bir küpe takan kız görülse de kızın kimliği etrafındaki spekülasyon, Vermeer’in metresi olmaktan onun 15 çocuğundan biri olmaya kadar uzanıyor.\r\n";
+                    break;
+                case "btnAndy":
+                    DigerleriniGizle("btnAndy", "pBAndy");
+                    richTextBoxAciklama.Visible = true;
+                    richTextBoxAciklama.Text = "Andy Warhol, “Campbell’s Soup Cans” (Campbell’in Çorba Konserveleri); 1962 " +
+                        " “Campbell’in Çorba Konserveleri”, şirket markası kullanarak Andy Warhol’un inanılmaz bir ün sağladığı bir dizi çalışmadan yalnızca bir tanesidir. Şablon kalıplar kullanılarak el ile yapılmış bu resmi dikkatli incelediğimizde bu konservelerin aralarındaki boşluklarının ya da görünümlerinin özdeş olmadığı gün yüzüne çıkıyor. Bir pop art sanatçısı olan Warhol’un 1962’de sunduğu 32 lezzetten birini temsil eden bu ünlü eseri, New York’taki The Museum of Modern Art’ta ziyaret edilebilir.\r\n";
+                    break;
+                case "btnPablo":
+                    DigerleriniGizle("btnPablo", "pBPablo");
+
+                    richTextBoxAciklama.Visible = true;
+                    richTextBoxAciklama.Text = "Pablo Picasso, “Guernica”; 1937 " +
+                        " Picasso’nun belirgin kübist stili içindeki politik bir protesto eseri olan “Guernica”, 1937’deki Paris Fuarı’nda öne çıkan parçalardan biriydi. Bu fuar için Picasso’dan bir çizim yapılması istenmiş ve Picasso, Kuzey İspanyol kasabası Guarnica’nın bombalanmasına kadar çizecek bir şey bulamamıştı. Bombalanmanın ardından ise çok kısa bir zaman içinde; Picasso’nun gri, siyah ve beyazdan oluşan geniş monokrom paletiyle yaratılan bu eser, olaylara bir tepki mahiyetindeydi. Hitler’in silahlı kuvvetleri tarafından gerçekleştirilen ve Franco hükümeti tarafından tasdik edilen bu saldırıyı anlatan Guernica tablosu, tarihteki en önemli isyan tablosu olarak Madrid’deki Kraliçe Sofia Ulusal Müzesi’nde bulunuyor.\r\n";
+                    break;
+            }
+        }
+              
+        
+        private void DigerleriniGizle(string buttonName, string pictureBoxName)
+        {
+            foreach (Control control in this.Controls)
+            {
+                // Button ve PictureBox isimleri hariç diğer kontrolleri gizle
+                if (control is Button && control.Name != buttonName)
+                {
+                    control.Visible = false;
+                }
+                else if (control is PictureBox && control.Name != pictureBoxName)
+                {
+                    control.Visible = false;
+                }
+                else if (!(control is Button) && !(control is PictureBox))
+                {
+                    control.Visible = false;
+                }
+            }
+                btnReset.Visible = true;
+        }
+        
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+                control.Visible = true;
+                richTextBoxAciklama.Visible = false;
+            }
+        
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void pBLeonardo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBoxAciklama_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
